3f083c1dfa2e4a3873175a35ebb76358ca81b55f
KA0420 <a.kaira0420@gmail.com>
2026-05-03 14:03:12 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const pug = require('pug');
 const server = http
   .createServer((req, res) => {
     const now = new Date();
-    console.info(`[${new Date()}] 次のIPアドレスからアクセスされたよ ${req.socket.remoteAddress}`
-    );
+    console.info(`次のIPアドレスからアクセスされたよ ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -51,7 +50,7 @@ const server = http
           .on('end', () => {
             const answer = new URLSearchParams(rawData);
             const body = `${answer.get('name')}さんは${answer.get('favorite')}に投票しました`;
-            console.info(`[${now}] 投稿: ${body}`);
+            console.info(`投稿: ${body}`);
             res.write(
             `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
             );
@@ -66,12 +65,12 @@ const server = http
     }
   })
   .on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
+    console.error(` Server Error`, e);
   })
   .on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
+    console.error(` Client Error`, e);
   });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
-  console.info(`[${new Date()}] Listening on ${port}`);
+  console.info(` Listening on ${port}`);
 });
